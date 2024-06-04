85ad972b1a69bfbbf6b6d62f39948a1a1eed6251
nidhal.jammali <nidhal.jammali@proxym-it.com>
2024-06-04 14:21:32 +0100

    Fix erreur

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,80 +1,82 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put, Query, Req, Res } from '@nestjs/common';
-import { Request, Response } from 'express';
-import { Todo } from './entities/todo.entity'; // Ensure this is the correct path
-import { count } from 'console';
-import { GetPaginatedTodoDto } from './todo/dto/get-all-todo.dto';
-import { AddTodoDto } from './dto/Add-Todo.Dto';
+import {Body, Controller, Delete, Get, NotFoundException, Param, Post, Put, Query, Req, Res} from '@nestjs/common';
+import {Request, Response} from 'express';
+import {Todo} from './entities/todo.entity'; // Ensure this is the correct path
+import {count} from 'console';
+import {GetAllTodoDto} from './dto/get-all-todo.dto';
+import {AddTodoDto} from './dto/Add-Todo.Dto';
 
 
 @Controller('/todo')
 export class TodoController {
-  private todos: Todo[] = [];
+    private todos: Todo[] = [];
 
 
-  @Get()
-  getTodos(@Req() request: Request, @Res() response: Response) {
-    console.log('recupere la liste');
-    response.status(200).json(this.todos);
-  }
-
-@Get('/detaill/:id')  
-getTodoById(@Param('id') id: string) {
-
-    const todo = this.todos.find((actualTodo) => actualTodo.id === +id);
-    if (todo) {
-      return todo;
+    @Get()
+    getTodos(@Req() request: Request, @Res() response: Response) {
+        console.log('recupere la liste');
+        response.status(200).json(this.todos);
     }
-    throw new NotFoundException(`le todo d'id ${id} n'existe pas`);
-  }
 
-  @Get('/search')
-getTodo (
-    @Query() mesQueryParams:GetPaginatedTodoDto,
+    @Get('/detaill/:id')
+    getTodoById(@Param('id') id: string) {
 
-){
-    console.log(mesQueryParams);
-    return this.todos;
+        const todo = this.todos.find((actualTodo) => actualTodo.id === +id);
+        if (todo) {
+            return todo;
+        }
+        throw new NotFoundException(`le todo d'id ${id} n'existe pas`);
+    }
+
+    @Get('/search')
+    getTodo(
+        @Query() mesQueryParams: GetAllTodoDto,
+    ) {
+        console.log(mesQueryParams);
+        return this.todos;
+    }
+
+
+    @Post()
+    AddTodos(@Body() newTodo: AddTodoDto) {
+        const todo = new Todo
+        const {name, description} = newTodo;
+        todo.name = name;
+        todo.description = description;
+        if (this.todos.length) {
+            todo.id = this.todos[this.todos.length - 1].id + 1;
+        } else {
+            todo.id = 1;
+        }
+        console.log(todo);
+        this.todos.push(todo);
+        return {message: 'Todo added', todo: newTodo};
+    }
+
+    //supprimer un todo par id
+    @Delete('/deleter/:id')
+    deleteTodo(@Param('id') id) {
+        const index = this.todos.findIndex((todo) => todo.id === +id);
+        //utliser la methode splice pour supprimer le todo chercher
+        if (index >= 0) {
+            this.todos.splice(index, 1);
+        } else {
+            throw new NotFoundException(`le todo d'id ${id} n'existe pas `)
+        }
+        return {
+            message: `le todo d'id ${id} à supprimer avec suucés `,
+            count: 1
+        };
+    }
+
+    @Put('/modifier/:id')
+    modifierTodo(@Param('id') id,
+                 @Body() newtodo: Partial<Todo>) {
+        const todo = this.getTodoById(id);
+        todo.description = newtodo.description ? newtodo.description : todo.description;
+        todo.name = newtodo.name ? newtodo.name : todo.name;
+
+        return todo;
+
+    }
 }
 
-
-  @Post()
-  AddTodos(@Body() newTodo: AddTodoDto) {
-    const todo= new Todo
-    const { name ,description}= newTodo;
-     todo.name=name;
-     todo.description=description;
-    if (this.todos.length) {
-      todo.id = this.todos[this.todos.length - 1].id + 1;
-    } else {
-      todo.id = 1;
-    }
-    console.log(todo);
-    this.todos.push(todo);
-    return { message: 'Todo added', todo: newTodo };
-  }
-  //supprimer un todo par id
-  @Delete ('/deleter/:id')
-  deleteTodo(@Param('id') id) {
-    const index=this.todos.findIndex((todo)=>todo.id === +id);
-    //utliser la methode splice pour supprimer le todo chercher
-    if(index>=0){
-        this.todos.splice(index,1);
-    }else{
-        throw new NotFoundException(`le todo d'id ${id} n'existe pas `)
-    }
-    return {
-        message:`le todo d'id ${id} à supprimer avec suucés `,
-        count:1
-    };
-    }
-
-  @Put('/modifier/:id')
-modifierTodo(@Param('id') id,
-@Body() newtodo:Partial<Todo>){
-    const todo=this.getTodoById(id);
-    todo.description=newtodo.description?newtodo.description : todo.description;
-    todo.name=newtodo.name?newtodo.name : todo.name;
-
-  return  todo;
-
-}}}
